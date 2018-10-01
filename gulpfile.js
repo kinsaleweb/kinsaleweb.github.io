0c23a49511a34dd45f17f76fde0aac78e5754046
Anant Anand Gupta <anantanandgupta@gmail.com>
2018-10-01 11:41:56 +0530

    fixed vulnerabilities

--- gulpfile.js
+++ gulpfile.js
@@ -17,55 +17,64 @@ var banner = ['/*!\n',
 ].join('');
 
 // Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function() {
+gulp.task('vendor',  gulp.series(
+    // Bootstrap
+    () => {
+      return gulp.src([
+          './node_modules/bootstrap/dist/**/*',
+          '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
+          '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
+        ])
+        .pipe(gulp.dest('./vendor/bootstrap'))
+    },
 
-  // Bootstrap
-  gulp.src([
-      './node_modules/bootstrap/dist/**/*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
-    ])
-    .pipe(gulp.dest('./vendor/bootstrap'))
+    // Font Awesome
+    () => {
+      return gulp.src([
+          './node_modules/font-awesome/**/*',
+          '!./node_modules/font-awesome/{less,less/*}',
+          '!./node_modules/font-awesome/{scss,scss/*}',
+          '!./node_modules/font-awesome/.*',
+          '!./node_modules/font-awesome/*.{txt,json,md}'
+        ])
+        .pipe(gulp.dest('./vendor/font-awesome'))
+    },
 
-  // Font Awesome
-  gulp.src([
-      './node_modules/font-awesome/**/*',
-      '!./node_modules/font-awesome/{less,less/*}',
-      '!./node_modules/font-awesome/{scss,scss/*}',
-      '!./node_modules/font-awesome/.*',
-      '!./node_modules/font-awesome/*.{txt,json,md}'
-    ])
-    .pipe(gulp.dest('./vendor/font-awesome'))
+    // jQuery
+    () => {
+      return gulp.src([
+          './node_modules/jquery/dist/*',
+          '!./node_modules/jquery/dist/core.js'
+        ])
+        .pipe(gulp.dest('./vendor/jquery'))
+    },
 
-  // jQuery
-  gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
-    .pipe(gulp.dest('./vendor/jquery'))
+    // jQuery Easing
+    () => {
+      return gulp.src([
+          './node_modules/jquery.easing/*.js'
+        ])
+        .pipe(gulp.dest('./vendor/jquery-easing'))
+    },
 
-  // jQuery Easing
-  gulp.src([
-      './node_modules/jquery.easing/*.js'
-    ])
-    .pipe(gulp.dest('./vendor/jquery-easing'))
+    // Magnific Popup
+    () => {
+      return gulp.src([
+          './node_modules/magnific-popup/dist/*'
+        ])
+        .pipe(gulp.dest('./vendor/magnific-popup'))
+    },
 
-  // Magnific Popup
-  gulp.src([
-      './node_modules/magnific-popup/dist/*'
-    ])
-    .pipe(gulp.dest('./vendor/magnific-popup'))
-
-  // Scrollreveal
-  gulp.src([
-      './node_modules/scrollreveal/dist/*.js'
-    ])
-    .pipe(gulp.dest('./vendor/scrollreveal'))
-
-});
+    // Scrollreveal
+    () => {
+      return gulp.src([
+        './node_modules/scrollreveal/dist/*.js'
+      ])
+      .pipe(gulp.dest('./vendor/scrollreveal'))
+    }));
 
 // Compile SCSS
-gulp.task('css:compile', function() {
+gulp.task('css:compile', function () {
   return gulp.src('./scss/**/*.scss')
     .pipe(sass.sync({
       outputStyle: 'expanded'
@@ -74,7 +83,7 @@ gulp.task('css:compile', function() {
 });
 
 // Minify CSS
-gulp.task('css:minify', ['css:compile'], function() {
+gulp.task('css:minify', gulp.series('css:compile', function () {
   return gulp.src([
       './css/*.css',
       '!./css/*.min.css'
@@ -85,13 +94,13 @@ gulp.task('css:minify', ['css:compile'], function() {
     }))
     .pipe(gulp.dest('./css'))
     .pipe(browserSync.stream());
-});
+}));
 
 // CSS
-gulp.task('css', ['css:compile', 'css:minify']);
+gulp.task('css', gulp.series('css:compile', 'css:minify'));
 
 // Minify JavaScript
-gulp.task('js:minify', function() {
+gulp.task('js:minify', function () {
   return gulp.src([
       './js/*.js',
       '!./js/*.min.js'
@@ -105,13 +114,13 @@ gulp.task('js:minify', function() {
 });
 
 // JS
-gulp.task('js', ['js:minify']);
+// gulp.task('js', gulp.series('js:minify'));
 
 // Default task
-gulp.task('default', ['css', 'js', 'vendor']);
+gulp.task('default', gulp.series(gulp.parallel('css', 'js:minify'), 'vendor'));
 
 // Configure the browserSync task
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function () {
   browserSync.init({
     server: {
       baseDir: "./"
@@ -120,8 +129,8 @@ gulp.task('browserSync', function() {
 });
 
 // Dev task
-gulp.task('dev', ['css', 'js', 'browserSync'], function() {
+gulp.task('dev', gulp.series(gulp.parallel('css', 'js:minify'), 'browserSync', function () {
   gulp.watch('./scss/*.scss', ['css']);
-  gulp.watch('./js/*.js', ['js']);
+  gulp.watch('./js/*.js', ['js:minify']);
   gulp.watch('./*.html', browserSync.reload);
-});
+}));
